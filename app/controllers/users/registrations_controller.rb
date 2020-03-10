5d11181f90735225e7438523249a7bc8c5c1fd75
papa-seiji <iwillbestandbyme@gmail.com>
2020-03-10 09:44:17 +0900

    プッシュします

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,9 @@
-protected
+# protected
  
-  def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :avatar, :avatar_cache, :remove_avatar])
-  end
+#   def configure_account_update_params
+#     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :avatar, :avatar_cache, :remove_avatar])
+#   end
 
-  def after_update_path_for(resource)
-    account_page_path
-  end
+#   def after_update_path_for(resource)
+#     account_page_path
+#   end
